3199fd54ab85fa0d155180c2e6875abda67fcdeb
RufatRahmatov <rehmetovruft@gmail.com>
2024-10-22 14:33:16 -0700

    components: header update

--- api-front/src/app/_components/header/index.tsx
+++ api-front/src/app/_components/header/index.tsx
@@ -1,12 +1,11 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Transition } from "@headlessui/react";
 import { BiMenu, BiX } from "react-icons/bi";
 import { IoIosArrowDown } from "react-icons/io";
 import { RiSearchLine } from "react-icons/ri";
-import { BsPerson } from "react-icons/bs";
-import { RiShoppingBag2Line } from "react-icons/ri";
 import { IoPersonOutline } from "react-icons/io5";
+import { RiShoppingBag2Line } from "react-icons/ri";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -32,7 +31,7 @@ const Header = () => {
 
   const handleLanguageChange = (language: string) => {
     setSelectedLanguage(language);
-    if (language === "Azərbaycan dili") {
+    if (language === "Azərbaycan") {
       setTextContent({
         helpline: "Yardım Nömrəsi: 123-456-7890",
         orders: "Yaz Kolleksiyası Satışından Alış-veriş edin!",
@@ -61,22 +60,47 @@ const Header = () => {
     }
   };
 
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+      if (!target.closest(".dropdown")) {
+        setOpenDropdown(null);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <header className="w-full ">
-      <div className="bg-black text-white text-sm flex justify-between px-4 py-2">
-        <div>{textContent.helpline}</div>
-        <div>{textContent.orders}</div>
+    <header className="w-full">
+      <div className="bg-black text-white text-sm flex justify-between px-4 py-3">
+        <div className="relative left-12">{textContent.helpline}</div>
+        <div className="">{textContent.orders}</div>
         <div className="flex items-center space-x-4">
-          {/* Currency Dropdown */}
-          <div className="relative">
+          <div className="relative dropdown">
             <button
               onClick={() => toggleDropdown("Currency")}
               className="inline-flex items-center"
             >
-              {selectedCurrency} <IoIosArrowDown className="ml-1" />
+              {selectedCurrency}{" "}
+              <IoIosArrowDown
+                className={`ml-1 transition-transform duration-300 ${
+                  openDropdown === "Currency" ? "rotate-180" : "rotate-0"
+                }`}
+              />
             </button>
-            {openDropdown === "Currency" && (
-              <div className="absolute right-0 mt-2 bg-white text-black py-2 px-4 shadow-lg rounded">
+            <Transition
+              show={openDropdown === "Currency"}
+              enter="transition ease-out duration-300 transform"
+              enterFrom="opacity-0 -translate-y-4"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-200 transform"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 -translate-y-4"
+            >
+              <div className="absolute right-0 mt-2 bg-black text-white py-2 px-4 shadow-lg rounded">
                 <div
                   className="cursor-pointer"
                   onClick={() => setSelectedCurrency("USD")}
@@ -90,33 +114,45 @@ const Header = () => {
                   AZN
                 </div>
               </div>
-            )}
+            </Transition>
           </div>
 
-          {/* Language Dropdown */}
-          <div className="relative">
+          <div className="relative dropdown">
             <button
               onClick={() => toggleDropdown("Language")}
-              className="inline-flex items-center"
+              className="inline-flex items-center mr-12"
             >
-              {selectedLanguage} <IoIosArrowDown className="ml-1" />
+              {selectedLanguage}{" "}
+              <IoIosArrowDown
+                className={`ml-1 transition-transform duration-300 ${
+                  openDropdown === "Language" ? "rotate-180" : "rotate-0"
+                }`}
+              />
             </button>
-            {openDropdown === "Language" && (
-              <div className="absolute right-0 mt-2 bg-white text-black py-2 px-4 shadow-lg rounded">
+            <Transition
+              show={openDropdown === "Language"}
+              enter="transition ease-out duration-300 transform"
+              enterFrom="opacity-0 -translate-y-4"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-200 transform"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 -translate-y-4"
+            >
+              <div className="absolute right-8 mt-2 bg-black text-gray-300 py-5 px-5 shadow-lg rounded font-medium">
                 <div
-                  className="cursor-pointer"
+                  className="cursor-pointer border-b-2 border-transparent hover:border-white"
                   onClick={() => handleLanguageChange("English")}
                 >
                   English
                 </div>
                 <div
-                  className="cursor-pointer mt-1"
-                  onClick={() => handleLanguageChange("Azərbaycan dili")}
+                  className="cursor-pointer mt-3 border-b-2 border-transparent hover:border-white"
+                  onClick={() => handleLanguageChange("Azərbaycan")}
                 >
-                  Azərbaycan dili
+                  Azərbaycan
                 </div>
               </div>
-            )}
+            </Transition>
           </div>
         </div>
       </div>
@@ -124,100 +160,58 @@ const Header = () => {
       <nav className="bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16 items-center">
-            <div className="flex-shrink-0">
-              <span className="text-2xl font-bold mr-32">Maxmod</span>
+            <div className=" relative flex justify-start">
+              <span className=" absolute right-[190px] text-2xl font-bold bottom-[-11px]">
+                Maxmod
+              </span>
             </div>
 
             <div className="hidden md:flex space-x-8">
-              <div className="relative">
-                <a
-                  href="#"
-                  className="hover:border-b-2 hover:border-black  font-medium text-lg"
-                >
-                  {textContent.home}
-                </a>
-              </div>
+              <a
+                href="#"
+                className="hover:border-b-2 hover:border-black font-medium text-lg"
+              >
+                {textContent.home}
+              </a>
 
-              <div className="relative flex items-center gap-x-1">
-                <span className="mx-1"></span>{" "}
-                <button
-                  onClick={() => toggleDropdown("Pages")}
-                  className="border-b-2 border-transparent hover:border-black font-medium text-lg inline-flex items-center"
-                >
-                  {textContent.pages}
-                </button>
-                <IoIosArrowDown className="mt-1 cursor-pointer " />
-                {openDropdown === "Pages" && (
-                  <Transition
-                    show={openDropdown === "Pages"}
-                    enter="transition-opacity duration-200"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="transition-opacity duration-150"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  ></Transition>
-                )}
-              </div>
+              <button
+                onClick={() => toggleDropdown("Pages")}
+                className="border-b-2 border-transparent hover:border-black font-medium text-lg inline-flex items-center"
+              >
+                {textContent.pages} <IoIosArrowDown className="ml-1" />
+              </button>
 
-              <div className="relative flex items-center gap-x-1">
-                <button
-                  onClick={() => toggleDropdown("Template")}
-                  className="border-b-2 border-transparent hover:border-black  font-medium text-lg inline-flex items-center"
-                >
-                  {textContent.template}
-                </button>
-                <IoIosArrowDown className="mt-1 cursor-pointer" />
-                {openDropdown === "Template" && (
-                  <Transition
-                    show={openDropdown === "Template"}
-                    enter="transition-opacity duration-200"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="transition-opacity duration-150"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute z-10 bg-white shadow-lg p-4 rounded text-lg">
-                      <ul>
-                        <li>Documentation</li>
-                        <li>Get Support</li>
-                      </ul>
-                    </div>
-                  </Transition>
-                )}
-              </div>
-
-              <div className="relative flex items-center gap-x-1">
-                <a
-                  href="#"
-                  className="border-b-2 border-transparent hover:border-black font-medium text-lg inline-flex items-center"
-                >
-                  {textContent.collection}
-                </a>
-                <IoIosArrowDown className="mt-1 cursor-pointer" />
-              </div>
+              <button
+                onClick={() => toggleDropdown("Template")}
+                className="border-b-2 border-transparent hover:border-black font-medium text-lg inline-flex items-center"
+              >
+                {textContent.template} <IoIosArrowDown className="ml-1" />
+              </button>
 
               <a
                 href="#"
-                className="hover:border-b-2 hover:border-black  font-medium text-lg"
+                className="hover:border-b-2 hover:border-black font-medium text-lg"
+              >
+                {textContent.collection}
+              </a>
+              <a
+                href="#"
+                className="hover:border-b-2 hover:border-black font-medium text-lg"
               >
                 {textContent.contactLens}
               </a>
               <a
                 href="#"
-                className="hover:border-b-2 hover:border-black  font-medium text-lg"
+                className="hover:border-b-2 hover:border-black font-medium text-lg"
               >
                 {textContent.eyeframes}
               </a>
             </div>
 
-            <div className="hidden md:flex items-center space-x-4">
+            <div className="hidden md:flex items-center space-x-4 relative left-[280px]">
               <RiSearchLine className="text-2xl cursor-pointer" />
               <IoPersonOutline className="text-2xl cursor-pointer" />
-              <div className="relative">
-                <RiShoppingBag2Line className="text-2xl cursor-pointer" />
-              </div>
+              <RiShoppingBag2Line className="text-2xl cursor-pointer" />
             </div>
 
             <div className="-mr-2 flex md:hidden">
@@ -236,6 +230,7 @@ const Header = () => {
           </div>
         </div>
 
+        {/* Mobile Menu */}
         {isOpen && (
           <div className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
@@ -279,10 +274,8 @@ const Header = () => {
 
             <div className="flex justify-around pb-4 border-t border-gray-200">
               <RiSearchLine className="text-2xl cursor-pointer font-medium" />
-              <BsPerson className="text-xl cursor-pointer font-medium" />
-              <div className="relative">
-                <RiShoppingBag2Line className="text-xl cursor-pointer font-medium " />
-              </div>
+              <IoPersonOutline className="text-2xl cursor-pointer font-medium" />
+              <RiShoppingBag2Line className="text-2xl cursor-pointer font-medium" />
             </div>
           </div>
         )}
