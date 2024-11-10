78a7d92a25af344f5bd223c1fee605473e5f6cd0
RufatRahmatov <rehmetovruft@gmail.com>
2024-11-10 12:54:01 -0800

    file update

--- api-front/src/app/_components/featuredcategory/index.tsx
+++ api-front/src/app/_components/featuredcategory/index.tsx
@@ -56,59 +56,67 @@ const categories: Category[] = [
 
 const FeaturedCategory: React.FC = () => {
   return (
-    <div className="p-4">
-      <div className="flex justify-between items-center mx-4">
-        <div>
-          <p className="text-sm text-gray-500">Eyeglasses Style</p>
-          <h2 className="text-2xl sm:text-4xl font-semibold mb-4">
-            Featured By Category
-          </h2>
+    <div className="relative -mt-[100px]">
+      <div className=" p-4 ">
+        <div className="flex justify-between items-center mx-8 mb-2">
+          <div className="mb-8">
+            <p className="text-md font-medium text-gray-500">
+              Eyeglasses Style
+            </p>
+            <h2 className="text-3xl sm:text-4xl font-medium ">
+              Featured By Category
+            </h2>
+          </div>
+          <button className="text-white bg-black font-medium border-2 border-black px-4 py-2 rounded-full hover:bg-white hover:text-black transition duration-300">
+            View Collection
+          </button>
         </div>
-        <button className="text-black border border-black px-4 py-2 rounded hover:bg-black hover:text-white transition">
-          View Collection
-        </button>
-      </div>
 
-      <div className="h-[280px] grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-6 mx-8 text-center">
-        {categories.map((category) => (
-          <div
-            key={category.id}
-            className="border rounded-lg shadow-md overflow-hidden"
-          >
-            <div className="p-4 overflow-hidden">
-              <img
-                src={category.image}
-                alt={category.title}
-                className="w-full h-[150px] object-cover rounded-lg mb-4 hover:transform transition-transform duration-300 group-hover:scale-110"
-              />
-              <h3 className="text-lg font-semibold">{category.title}</h3>
-              <p className="text-gray-500 text-sm">
-                {category.itemsCount} items
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
+        <div className="h-[270px] grid grid-cols-2  sm:grid-cols-3 lg:grid-cols-6 gap-6 mx-8 text-center">
+          {categories.map((category) => (
+            <div
+              key={category.id}
+              className="border-2 border-gray-300 rounded-xl  overflow-hidden"
+            >
+              <div className="p-4 rounded-lg">
+                <div className="overflow-hidden rounded-lg  h-[165px]">
+                  {" "}
+                  <img
+                    src={category.image}
+                    alt={category.title}
+                    className="w-full h-[165px] object-cover rounded-lg mb-4 transform transition-transform duration-300 hover:scale-105 cursor-pointer"
+                  />
+                </div>
 
-      <div className="flex overflow-x-auto space-x-4 sm:hidden mt-4">
-        {categories.map((category) => (
-          <div
-            key={category.id}
-            className="flex-shrink-0 w-48 border rounded-lg shadow-md"
-          >
-            <div className="p-4">
-              <img
-                src={category.image}
-                alt={category.title}
-                className="w-full h-32 object-cover mb-4"
-              />
-              <h3 className="text-lg font-semibold">{category.title}</h3>
-              <p className="text-gray-500 text-sm">
-                {category.itemsCount} items
-              </p>
+                <h3 className="text-lg font-semibold">{category.title}</h3>
+                <p className="text-gray-500 text-sm">
+                  {category.itemsCount} items
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+
+        <div className="flex overflow-x-auto space-x-4 sm:hidden mt-4">
+          {categories.map((category) => (
+            <div
+              key={category.id}
+              className="flex-shrink-0 w-48 border rounded-lg shadow-md"
+            >
+              <div className="p-4">
+                <img
+                  src={category.image}
+                  alt={category.title}
+                  className="w-full h-32 object-cover mb-4"
+                />
+                <h3 className="text-lg font-semibold">{category.title}</h3>
+                <p className="text-gray-500 text-sm">
+                  {category.itemsCount} items
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
